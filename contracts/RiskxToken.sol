c2d921183f0e2aea18d725bb8fb3188151237da4
hotsiam <zanemassey@hotmail.com>
2018-07-27 13:01:37 +1000

    3. delegated Transfer

--- contracts/RiskxToken.sol
+++ contracts/RiskxToken.sol
@@ -11,8 +11,17 @@ contract RiskxToken {
 		address indexed _to, 
 		uint256 _value
 	);
+	// Transfer event
+
+	event Approval( 
+		address indexed _owner,
+		address indexed _spender,
+		uint256 _value
+	);
 
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
+	// Allowance
 
 	function RiskxToken (uint256 _initialSupply) public {
 		balanceOf[msg.sender] = _initialSupply;
@@ -32,4 +41,29 @@ contract RiskxToken {
 	// Transfer Event
 		return true;
 	}
+	// approve function
+	function approve(address _spender, uint256 _value) public returns (bool success) {
+		// allowance
+		allowance[msg.sender][_spender] = _value;
+
+		// approve event
+		Approval(msg.sender, _spender, _value);
+
+		return true;
+	}
+
+	// Transfer from function
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+		require(_value <= balanceOf[_from]);
+		require(_value <= allowance[_from][msg.sender]);
+		balanceOf[_from] -= _value;
+		balanceOf[_to] += _value;
+
+		allowance[_from][msg.sender] -= _value;
+
+		Transfer(_from, _to, _value);
+
+		return true;
+	}
 }
+	
