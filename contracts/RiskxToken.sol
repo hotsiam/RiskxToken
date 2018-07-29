f4739d1fdfd134e5f33f76f1b02ad4dc0a70c740
hotsiam <zanemassey@hotmail.com>
2018-07-29 21:52:04 +1000

    safe math + function buy tokens

--- contracts/RiskxToken.sol
+++ contracts/RiskxToken.sol
@@ -1,69 +1,61 @@
 pragma solidity ^0.4.23;
 
 contract RiskxToken {
-	string  public name = "Riskx Token";
-	string  public symbol = "RIX";
-	string  public standard = "Riskx Token v1.0";
-	uint256 public totalSupply;
+    string  public name = "Riskx Token";
+    string  public symbol = "RIX";
+    string  public standard = "Riskx Token v1.0";
+    uint256 public totalSupply;
 
-	event Transfer(
-		address indexed _from,
-		address indexed _to, 
-		uint256 _value
-	);
-	// Transfer event
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
 
-	event Approval( 
-		address indexed _owner,
-		address indexed _spender,
-		uint256 _value
-	);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
-	mapping(address => uint256) public balanceOf;
-	mapping(address => mapping(address => uint256)) public allowance;
-	// Allowance
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-	function RiskxToken (uint256 _initialSupply) public {
-		balanceOf[msg.sender] = _initialSupply;
-		totalSupply = _initialSupply;
-	}
-	// Transfer function
+    function RiskxToken (uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
 
-		function transfer(address _to, uint256 _value) public returns (bool success) {
-	// Exception if account doesnt have enough
-		require(balanceOf[msg.sender] >= _value);
-		balanceOf[msg.sender] -= _value;
-		balanceOf[_to] += _value;
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
 
-		Transfer(msg.sender, _to, _value); 
-	// Transfer event
-	// Return a boolean
-	// Transfer Event
-		return true;
-	}
-	// approve function
-	function approve(address _spender, uint256 _value) public returns (bool success) {
-		// allowance
-		allowance[msg.sender][_spender] = _value;
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
 
-		// approve event
-		Approval(msg.sender, _spender, _value);
+        Transfer(msg.sender, _to, _value);
 
-		return true;
-	}
+        return true;
+    }
 
-	// Transfer from function
-	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-		require(_value <= balanceOf[_from]);
-		require(_value <= allowance[_from][msg.sender]);
-		balanceOf[_from] -= _value;
-		balanceOf[_to] += _value;
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
 
-		allowance[_from][msg.sender] -= _value;
+        Approval(msg.sender, _spender, _value);
 
-		Transfer(_from, _to, _value);
+        return true;
+    }
 
-		return true;
-	}
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        Transfer(_from, _to, _value);
+
+        return true;
+    }
 }
-	
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var RiskxToken = artifacts.require("./RiskxToken.sol");
+var RiskxTokenSale = artifacts.require("./RiskxTokenSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(RiskxToken, 600000000);
+    deployer.deploy(RiskxToken, 600000000).then(function() {
+    	// Token price is 0.00015 Ether or 0.070 USD
+    	var tokenPrice = 60000000000000; 
+  		return deployer.deploy(RiskxTokenSale, RiskxToken.address, tokenPrice); 
+  	});
 };
